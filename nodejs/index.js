012083ed7963d46a47da9d3d3501c6ebcc27b2f8
Greenman23 <jrobertlott@gmail.com>
2019-10-03 13:56:07 -0400

    Still working on db problem
    Moved all the db methods to one class.

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,79 +1,20 @@
 'use strict';
 
+
+const config = require('./config.json');
 const http = require('http');
-const fs = require('fs');
-
-function login_verfication_request(Username, Password, connection){
-
-    var verification = 'SELECT verify_user("' + Username + '","' + Password + '");';
-
-    connection.query(verification, function(error,results,feilds){
-        console.log(results)
-    });
-}
-
-function add_user(FirstName, LastName, Username, Password, Gender, BirthDate, connection){
-    var new_user = 'SELECT insert_user("' + FirstName + '","' + LastName + '","' + Username + 
-    '","' + Password + '","' + Gender + '", DATE("' + BirthDate + '"));';
-
-    connection.query(new_user, function(error,results,feilds){
-        console.log(results);   
-    });
-}
-
-
-function insert_survey(SurveyName, SurveyDescription, SurveyVersion, connection){
-
-    var new_survey = 'SELECT insert_survey("' + SurveyName + '","' + SurveyDescription + '","' + SurveyVersion + '");';
-
-    connection.query(new_survey,function(error,results,feilds){
-        console.log(results);
-    });
-}
-
-function insert_survey_version(SurveyVersionName,SurveyDescription,connection){
-    var survey_ver = 'SELECT insert_survey_version("' + SurveyVersionName + '","' + SurveyDescription + '");'
-
-    connection.query(qeustion_ver,function(error,results,feilds){
-        console.log(results);
-    });
-}
-
-function insert_question_version(QuestionName, Question, connection){
-    var qeustion_ver = 'SELECT insert_question_version("' + QuestionName + '","' + Question + '");'
-
-    connection.query(qeustion_ver,function(error,results,feilds){
-        console.log(results);
-    });
-}
-
-function insert_question(Question, Answer, QuestionType, QuestionVersion, connection){
-    var new_question = 'SELECT insert_question("' + Question + '","' + Answer + '","' + QuestionType + '","'  + QuestionVersion +'");';
-
-    connection.query(new_question,function(error,results,feilds){
-        console.log(results);
-    });
-
-}
-
-
-function get_all_surveys(connection){
-    var all_surveys = 'SELECT get_surveys();'
-
-    connection.query(new_question,function(error,results,feilds){
-        console.log(results);
-    });
-}
-
-function get_all_questions(connection){
-    var all_questions = 'SELECT get_questions();';
-
-    connection.query(all_questions,function(error,results,feilds){
-        console.log(results);
-    });
+const mysql = require('mysql');
+const quary = require('./sql_quaries');
+const conInfo = {
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.name
 }
 
 var app = http.createServer(function(request, response){ 
+
+    var sql_response = "";
 
     if(request.method == 'POST') {
         console.log("Post Request Received from ip address=>" + request.connection.remoteAddress + " processing request....")
@@ -95,54 +36,23 @@ var app = http.createServer(function(request, response){
                         console.log(post.Username, post.Password);
                     }
                 }
-
-
-                //Beggining of DB Portion
-                const config = require('./config.json')
-
-                let mysql = require('mysql');
                 
-                let connection = mysql.createConnection({
-                    host: config.host,
-                    user: config.user,
-                    password: config.password,
-                    database: config.name
-                });
-                
-                connection.connect(function (err) {
-                    if (err) {
-                        return console.error('error: ' + err.message);
-                    }
-                
-                    console.log('Connection to the ' + config.name + ' has been made....');
-                });
+                let connection = mysql.createConnection(conInfo);
                 
                 if(post.FirstName != undefined){
                     var TestDate = "2001-09-11"
                     console.log(TestDate)
-                    add_user(post.FirstName,post.LastName,post.Username,post.Password,post.Gender,TestDate, connection)
+                    quary.signup(post.FirstName,post.LastName,post.Username,post.Password,post.Gender,TestDate, connection)
                 }
 
                 if(post.Username != undefined && post.Password != undefined){
-                    login_verfication_request(post.Username, post.Password, connection);
+                  sql_response =  quary.login(post.Username, post.Password, connection);
                 }
                 
-                connection.end(function(err) {
-                  if (err) {
-                    return console.log('error:' + err.message);
-                  }
-                  console.log('Close the database connection.');
-                });
-                //End of DB Portion
-
-                //deal_with_post_data(request,post)
-                var dict= {
-                    "Answer" : 1
-                }
-                console.log(dict);
-                var dictstring = JSON.stringify(dict);
+                var dictstring = JSON.stringify(sql_response);
                 response.writeHead(200, {"Content-Type" : "application/json"})
                 response.end(dictstring);
+  
             }
             catch(err){
                 console.log(err)
---
+++ nodejs/sql_quaries.js
@@ -0,0 +1,114 @@
+
+function createConnection(Connection, QueryString, callback){
+    Connection.connect(function(err) {
+        if(error) throw error;
+
+        makeQuary 
+
+    });
+}
+
+function makeQuary(QueryString){
+       
+    Connection.query(QueryString, function (error, result) {
+        if (error) {
+            callback(error,null)
+        };
+        
+        callback(null,result)
+      });
+}
+
+module.exports  = {
+
+    login: function(Username, Password, connection, callback){
+
+        var verification = 'SELECT verify_user("' + Username + '","' + Password + '");';
+
+        var resp_sql = "";
+
+        db_quary = createConnection(connection,verification);
+
+        var auth;
+
+        for (let value of Object.values(db_quary[0])) {
+            auth = value;
+        }
+
+        if(auth == 1){
+            resp_sql = {
+                "Authentication" : "Succeded"
+            };
+        }
+
+        else {
+            resp_sql = {
+                "Authentication" : "Failure"
+            };
+        }
+
+        return resp_sql;
+    
+    },
+
+    signup: function(FirstName, LastName, Username, Password, Gender, BirthDate, connection){
+        var new_user = 'SELECT insert_user("' + FirstName + '","' + LastName + '","' + Username + 
+        '","' + Password + '","' + Gender + '", DATE("' + BirthDate + '"));';
+
+        connection.query(new_user, function(error,results,feilds){
+            console.log(results);   
+        });
+    },
+
+
+    add_survey: function (SurveyName, SurveyDescription, SurveyVersion, connection){
+
+        var new_survey = 'SELECT insert_survey("' + SurveyName + '","' + SurveyDescription + '","' + SurveyVersion + '");';
+
+        connection.query(new_survey,function(error,results,feilds){
+            console.log(results);
+        });
+    },
+
+    new_survey_version: function (SurveyVersionName,SurveyDescription,connection){
+        var survey_ver = 'SELECT insert_survey_version("' + SurveyVersionName + '","' + SurveyDescription + '");'
+
+        connection.query(qeustion_ver,function(error,results,feilds){
+            console.log(results);
+        });
+    },
+
+    new_question_version: function(QuestionName, Question, connection){
+        var qeustion_ver = 'SELECT insert_question_version("' + QuestionName + '","' + Question + '");'
+
+        connection.query(qeustion_ver,function(error,results,feilds){
+            console.log(results);
+        });
+    },
+
+    new_question: function (Question, Answer, QuestionType, QuestionVersion, connection){
+        var new_question = 'SELECT insert_question("' + Question + '","' + Answer + '","' + QuestionType + '","'  + QuestionVersion +'");';
+
+        connection.query(new_question,function(error,results,feilds){
+            console.log(results);
+        });
+
+    },
+
+
+    get_all_surveys: function (connection){
+        var all_surveys = 'SELECT get_surveys();'
+
+        connection.query(new_question,function(error,results,feilds){
+            console.log(results);
+        });
+    },
+
+    get_all_questions: function(connection){
+        var all_questions = 'SELECT get_questions();';
+
+        connection.query(all_questions,function(error,results,feilds){
+            console.log(results);
+        });
+    }
+}
