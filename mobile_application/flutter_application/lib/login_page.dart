18ab83f2bccecba0b07ca270fb7421d4eaeff7e9
Greenman23 <jrobertlott@gmail.com>
2019-10-02 20:05:42 -0400

    Start of Login Page
    Created Start of Login Page

---
+++ mobile_application/flutter_application/lib/login_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: SafeArea(
+      ),
+    );
+  }
+
+}
