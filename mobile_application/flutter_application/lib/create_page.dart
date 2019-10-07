ee698bc6a5663f6f959469021780f86368d808ed
Thinker333 <ericschneider415@gmail.com>
2019-10-07 15:24:28 -0400

    Made picture screen look more like actual mock up

--- mobile_application/flutter_application/lib/create_page.dart
+++ mobile_application/flutter_application/lib/create_page.dart
@@ -102,11 +102,7 @@ class CreatePageState extends State<CreatePage> {
         if (success) {
           widget.outerConfig.username = innerConfig.username;
           widget.outerConfig.password = innerConfig.password;
-          Navigator.of(context).push(MaterialPageRoute(
-              settings: RouteSettings(name: "/profilepicture"),
-              builder: (BuildContext context) {
-                return ProfilePic(config: widget.outerConfig);
-              }));
+
         }
         update();
       });
@@ -160,6 +156,11 @@ class CreatePageState extends State<CreatePage> {
               createAccountColor = Colors.red;
               update();
             } else {
+              Navigator.of(context).push(MaterialPageRoute(
+                  settings: RouteSettings(name: "/profilepicture"),
+                  builder: (BuildContext context) {
+                    return ProfilePic(config: widget.outerConfig);
+                  }));
               _signupHttp();
             }
           }),
