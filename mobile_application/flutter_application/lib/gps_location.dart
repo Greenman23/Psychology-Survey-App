e4dc79aafe6b92ef659c0c9bcb43be96e64b7de1
Greenman23 <jrobertlott@gmail.com>
2019-10-10 00:28:12 -0400

    Location is no recorded in config

--- mobile_application/flutter_application/lib/gps_location.dart
+++ mobile_application/flutter_application/lib/gps_location.dart
@@ -108,34 +108,35 @@ class _GPSLocationState  extends State<GPSLocation> {
         'Do you want to provide your location?';
 
     //TODO: Add logic for if the user does not enable location handling
-    //TODO: Add user location information to config
-    return Stack(
-      children: <Widget>[
-        new Container(
-          height: 250,
-          alignment: Alignment.topCenter,
-          child: worldMap,
-        ),
-        new Container(
-          alignment: Alignment.center,
-          child: new Text(msg),
-        ),
-        new Container(
-          alignment: Alignment.bottomLeft,
-          child: getPaddedButton(
-              "Yes", () {
-                Navigator.popUntil(this.context, ModalRoute.withName("/"));
-          }),
-        ),
-        new Container(
-          alignment: Alignment.bottomRight,
-          child: getPaddedButton(
-              "No", () {
-            Navigator.popUntil(this.context, ModalRoute.withName("/"));
-          }),
-        )
-      ],
-    );
+      return Stack(
+        children: <Widget>[
+          new Container(
+            height: 250,
+            alignment: Alignment.topCenter,
+            child: worldMap,
+          ),
+          new Container(
+            alignment: Alignment.center,
+            child: new Text(msg),
+          ),
+          new Container(
+            alignment: Alignment.bottomLeft,
+            child: getPaddedButton(
+                "Yes", () {
+                  widget.config.loc = currentLocation;
+              Navigator.popUntil(this.context, ModalRoute.withName("/"));
+            }),
+          ),
+          new Container(
+            alignment: Alignment.bottomRight,
+            child: getPaddedButton(
+                "No", () {
+              widget.config.loc = null;
+              Navigator.popUntil(this.context, ModalRoute.withName("/"));
+            }),
+          )
+        ],
+      );
   }
 
   Widget build(BuildContext) {
