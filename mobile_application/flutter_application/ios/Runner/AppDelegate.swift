35c12eec938435dc4e75a99a4a588096c16796a1
Greenman23 <jrobertlott@gmail.com>
2019-10-14 16:11:59 -0400

    manefest is now fi ignored
    This is so we can we dont push api keys

--- mobile_application/flutter_application/ios/Runner/AppDelegate.swift
+++ mobile_application/flutter_application/ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR KEY HERE")
+    GMSServices.provideAPIKey("AIzaSyBE6U-NUusIu7gZm8KfzASRU_t3uJJJyWE")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
