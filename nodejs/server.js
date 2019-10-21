68ce822d3b40e12ec118ec0dcac7aa1bcabfb173
Greenman23 <jrobertlott@gmail.com>
2019-10-21 13:57:57 -0400

    Correct Spelling errors

--- nodejs/server.js
+++ nodejs/server.js
@@ -4,7 +4,7 @@
 const config = require('./config.json');
 const http = require('http');
 const mysql = require('mysql');
-const quary = require('./sql_queries');
+const query = require('./sql_queries');
 const conInfo = {
     host: config.host,
     user: config.user,
@@ -15,7 +15,7 @@ const conInfo = {
 function sendJSON(request,response, msg){
     var dictstring = JSON.stringify(msg);
     response.writeHead(200, {"Content-Type" : "application/json"})
-    console.log("Response sent to =>" + request.connection.remoteAddress + "....")
+    console.log("Response sent to =>" + request.connection.remoteAddress + ".")
     response.end(dictstring);
 }
 
@@ -24,7 +24,7 @@ var app = http.createServer(function(request, response){
     var sql_response = "";
 
     if(request.method == 'POST') {
-        console.log("Post Request Received from ip address=>" + request.connection.remoteAddress + " processing request....")
+        console.log("Post Request Received from ip address=>" + request.connection.remoteAddress + " processing request.")
         var body = '';
         request.on('data', function(data){
             body+= data;
@@ -40,24 +40,24 @@ var app = http.createServer(function(request, response){
                 
                 if(post.Type == 'signup'){
 
-                    quary.signup(post.FirstName,post.LastName,post.Username,post.Password,post.Gender,post.BirthDate, connection, function(signup_resp){
+                    query.signup(post.FirstName,post.LastName,post.Username,post.Password,post.Gender,post.BirthDate, connection, function(signup_resp){
                         sendJSON(request,response,signup_resp);
                     });
                 }
 
                 else if(post.Type == 'login'){
-                  quary.login(post.Username, post.Password, connection, function(my_response){
+                  query.login(post.Username, post.Password, connection, function(my_response){
                         sendJSON(request,response,my_response);
                   });
                 }
                 else if(post.Type == 'getSurveys'){
-                  quary.get_all_surveys(connection, function(get_survey_response){
+                  query.get_all_surveys(connection, function(get_survey_response){
                         sendJSON(request,response, get_survey_response);
                   });
                 }
 
                 else if(post.Type == 'getQuestionsForSurvey'){
-                    quary.get_survey_questions(post.SurveyName,connection, function(surveyQuestions){
+                    query.get_survey_questions(post.SurveyName,connection, function(surveyQuestions){
                         sendJSON(request,response,surveyQuestions);
                   });
                 }
@@ -86,4 +86,4 @@ var app = http.createServer(function(request, response){
 });
 
 app.listen(80);
-console.log("Http server is now running....")
+console.log("Http server is now running.")
