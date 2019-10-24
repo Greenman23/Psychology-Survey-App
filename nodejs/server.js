b027a20cfe97f3313bed244f5a437a5268d656dc
Greenman23 <jrobertlott@gmail.com>
2019-10-24 11:13:41 -0400

    Cleaned up server code
    I cleaned up the code and removed unused variables

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const config = require('./config.json');
 const http = require('http');
 const mysql = require('mysql');
@@ -11,40 +10,36 @@ const conInfo = {
     password: config.password,
     database: config.name
 }
-
 function sendJSON(request,response, msg){
     var dictstring = JSON.stringify(msg);
     response.writeHead(200, {"Content-Type" : "application/json"})
     console.log("Response sent to =>" + request.connection.remoteAddress + ".")
     response.end(dictstring);
 }
-
 var app = http.createServer(function(request, response){ 
-
-    var sql_response = "";
-
     if(request.method == 'POST') {
         console.log("Post Request Received from ip address=>" + request.connection.remoteAddress + " processing request.")
-        var body = '';
+        var requestBody = '';
         request.on('data', function(data){
-            body+= data;
+            requestBody+= data;
         })
-
         request.on('end', function(){
             try {
                 if('application/json' === request.headers['content-type']){
-                    var post = JSON.parse(body);
+                    var post = JSON.parse(requestBody);
                 }
-                
+                else{
+                    response.writeHead(200, {"Content-Type" : "application/json"})
+                    console.log("Response sent to =>" + request.connection.remoteAddress + ".")
+                    response.end("This server only recives json request.");
+                }     
                 let connection = mysql.createConnection(conInfo);
-                
                 if(post.Type == 'signup'){
 
                     query.signup(post.FirstName,post.LastName,post.Username,post.Password,post.Gender,post.BirthDate, connection, function(signup_resp){
                         sendJSON(request,response,signup_resp);
                     });
                 }
-
                 else if(post.Type == 'login'){
                   query.login(post.Username, post.Password, connection, function(my_response){
                         sendJSON(request,response,my_response);
@@ -55,27 +50,22 @@ var app = http.createServer(function(request, response){
                         sendJSON(request,response, get_survey_response);
                   });
                 }
-
                 else if(post.Type == 'getQuestionsForSurvey'){  
                     query.get_survey_questions(post.SurveyName,connection, function(surveyQuestions){
                         sendJSON(request,response,surveyQuestions);
                   });
                 }
-
                 else if(post.Type == 'answers'){  
                     query.send_answers(post.Username, post.Password, connection, post.Map, function(status){
                         sendJSON(request,response,status);
                   });
                 }
-
-
                 else {
                     var error_request = {
                         "Error" : "Bad Request",
                     }
                     sendJSON(request,response,error_request);
                 }
-
                 connection.end();
             }
             catch(err){
@@ -84,13 +74,11 @@ var app = http.createServer(function(request, response){
             }
         })
     }
-
     else {
         console.log("A non-POST request was recieved")
         response.write("Sorry this webserver only receives post request. The website will be developed later.")
         response.end();
     }
 });
-
 app.listen(80);
 console.log("Http server is now running.")
--- nodejs/sql_queries.js
+++ nodejs/sql_queries.js
@@ -2,76 +2,58 @@
 function query(q, connection, callback){
     connection.query(q, function(error,results,[])
     {
-        if(error) throw error;
+        if(error) throw console.error(error);
         callback(null,results)
     });
 }
-
 function insert_query(q, connection, callback){
     connection.query(q, function(error)
     {
-        if(error) throw error;
-
-        callback("Success")
+        var insert_status = {
+            'Process' : 'Sucessful',
+        }
+        if(error) throw console.error(error);
+        callback(insert_status)
     });
 }
-
 function get_user_id(Username, Password, connection, callback){
-    
-    console.log("Username: ", Username, "Password: ", Password)
-
     var get_user_id = 'CALL get_user_id_answer("' + Username + '","' + Password + '");';
-
     query(get_user_id, connection, function(error,res){
-
         var userid;
-
         if(error) throw error   
-
         for (let value of Object.values(res[0])) {
             userid = value.pk_user_id;
         }
         callback(userid);
     });
 }
-
 module.exports  = {
-
     login: function(Username, Password, connection, callback){
-
         if(Username != undefined && Password != undefined){
             var verification = 'SELECT verify_user("' + Username + '","' + Password + '");';
-            
             var resp_sql = "";
-
             var auth;
-
             auth = 0;
-
             query(verification, connection, function(error,res){
                 if(error) throw error   
                 for (let value of Object.values(res[0])) {
                     auth = value;
                 }
-                
                 if(auth == 1){
                     resp_sql = {
                         "Authentication" : true,
                         "Hash" : 12345
                     };
                 }
-        
                 else {
                     resp_sql = {
                         "Authentication" : false,
                         "Hash" : 12345
                     };
                 }
-
                 callback(resp_sql)
             });
         }
-
         else {
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
@@ -79,32 +61,23 @@ module.exports  = {
             callback(error_resp)
         }
     },
-
     signup: function(FirstName, LastName, Username, Password, Gender, BirthDate, connection, callback){
-
         if(FirstName != undefined && LastName != undefined && Username != undefined && Password != undefined && Gender != undefined &&
             BirthDate !=undefined ) {
-
             var new_user = 'SELECT insert_user("' + FirstName + '","' + LastName + '","' + Username + 
             '","' + Password + '","' + Gender + '", DATE("' + BirthDate + '"));';
-
-
             var sql_resp="No Response"
-
             var auth = 0;
             query(new_user, connection, function(error,res){
-                if(error) throw error   
-
+                if(error) throw console.error(error)   
                 for (let value of Object.values(res[0])) {
                     auth = value;
                 }
-
                 if(auth == 1){
                     sql_resp = {
                         "Account_Creation" : true,
                     }
                 }
-
                 else {
                     sql_resp = {
                         "Account_Creation" : false,
@@ -114,8 +87,7 @@ module.exports  = {
                 console.log("Response: " + sql_resp);
                 callback(sql_resp);
             });
-        }
-        
+        }    
         else {
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
@@ -123,17 +95,12 @@ module.exports  = {
             callback(error_resp)
         }
     },
-
      get_all_surveys: function(connection, callback){
         var all_surveys = 'CALL get_surveys();'
         var jsonresponse
-        var get_survey_response = "No Response"
         var array = []
         query(all_surveys, connection, function(error, res){
-            if (error) throw error;
-            
-            value = 0;
-            
+            if (error) throw console.error(error);
             for (let value of Object.values(res[0])) {
                 array.push(value.survey_name)
             }
@@ -144,20 +111,14 @@ module.exports  = {
         });
 
     },
-
     get_survey_questions: function(survey, connection, callback){
-
         if(survey != undefined){
             var questions = 'CALL get_questions_by_survey("' + survey + '");';
-
             var suveyQuestions = {"Questions": []}
-
             jsonResponse="";
-
             query(questions, connection, function(error,res){
                 jsonResponse = ""
-                if(error) throw error;
-
+                if(error) throw console.error(error);
                 for (let value of Object.values(res[0])) {
                     value.answers = value.answers.split("'").join("\"")
                     value.answers = ('{"data" : ' + value.answers + '}')
@@ -173,42 +134,27 @@ module.exports  = {
                     }
                     suveyQuestions['Questions'].push(temp)
                 }
-
                 callback(suveyQuestions);  
             });
         }
         else {
-
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
             }
             callback(error_resp)
-
         }       
     },
-
     send_answers: function(user, pass, connection, answers, callback){
-
-
-        if(user != undefined && pass != undefined && answers != undefined ){
-
-            
+        if(user != undefined && pass != undefined && answers != undefined ){  
             for(i = 0; i < answers.Questions.length; i++){
-
                 var answer = 'CALL insert_answer("' + user + '","' + pass + '",' +  answers.Questions[i].ID + ',"' + answers.Questions[i].UserAnswer + '");';
                 insert_query(answer, connection, function(res){
-
-                    console.log("Result ", res);
-                    console.log("_____________________________________________________")
                 });
             }
-            callback("done")
-
-           /* get_user_id(user,pass, connection, function(userID){
-                console.log(userID);
-
-
-            });*/
+            var done_rep = {
+                "Process" : "Done",
+            };
+            callback(done_rep);
         }
         else {
             var error_resp = {
@@ -217,5 +163,4 @@ module.exports  = {
             callback(error_resp)
         }       
     },
-
 }
