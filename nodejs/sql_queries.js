643177d0ef36d1af0b76d604803a758682aa188d
Greenman23 <jrobertlott@gmail.com>
2019-11-01 15:33:45 -0400

    Server now sends back user info after loggin

--- nodejs/sql_queries.js
+++ nodejs/sql_queries.js
@@ -1,63 +1,53 @@
 
-function query(q, connection, callback){
+/*function query(q, connection, callback){
     connection.query(q, function(error,results,[])
     {
         if(error) throw console.error(error);
+        console.log(results)
         callback(null,results)
     });
-}
-function insert_query(q, connection, callback){
-    connection.query(q, function(error)
-    {
-        var insert_status = {
-            'Process' : 'Sucessful',
-        }
-        if(error) throw console.error(error);
-        callback(insert_status)
-    });
-}
-function get_user_id(Username, Password, connection, callback){
-    var get_user_id = 'CALL get_user_id_answer("' + Username + '","' + Password + '");';
-    query(get_user_id, connection, function(error,res){
-        var userid;
-        if(error) throw error   
-        for (let value of Object.values(res[0])) {
-            userid = value.pk_user_id;
-        }
-        callback(userid);
-    });
-}
+}*/
 module.exports  = {
     login: function(Username, Password, connection, callback){
         if(Username != undefined && Password != undefined){
-            var verification = 'SELECT verify_user("' + Username + '","' + Password + '");';
-            var resp_sql = "";
-            var auth;
-            auth = 0;
-            query(verification, connection, function(error,res){
+            var verification = 'SELECT verify_user("' + Username + '","' + Password + '");'
+            var auth = 0
+            connection.query(verification, function(error,res){
                 if(error) throw error   
                 for (let value of Object.values(res[0])) {
                     auth = value;
                 }
                 if(auth == 1){
-                    resp_sql = {
-                        "Authentication" : true,
-                        "Hash" : 12345
-                    };
+                    var get_user_information = 'SELECT first_name, last_name, sex, age FROM USERS WHERE user_name="' + Username + 
+                    '" AND user_password="' + Password + '";'
+                    connection.query(get_user_information, function(error,results)
+                    {
+                        if(error) throw console.error(error);
+                        var user_info = {
+                            'first_name' : results[0].first_name,
+                            'last_name' : results[0].last_name,
+                            'Gender' : results[0].sex,
+                            'DOB' : results[0].age,
+                        }
+                        connection.end()
+                        callback(user_info)
+                    });
                 }
                 else {
-                    resp_sql = {
+                    var error_resp = {
                         "Authentication" : false,
                         "Hash" : 12345
                     };
+                    connection.end()
+                    callback(error_resp)
                 }
-                callback(resp_sql)
             });
         }
         else {
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
             }
+            connection.end()
             callback(error_resp)
         }
     },
@@ -65,11 +55,11 @@ module.exports  = {
         if(FirstName != undefined && LastName != undefined && Username != undefined && Password != undefined && Gender != undefined &&
             BirthDate !=undefined ) {
             var new_user = 'SELECT insert_user("' + FirstName + '","' + LastName + '","' + Username + 
-            '","' + Password + '","' + Gender + '", DATE("' + BirthDate + '"));';
-            var sql_resp="No Response"
+            '","' + Password + '","' + Gender + '", DATE("' + BirthDate + '"));'
             var auth = 0;
-            query(new_user, connection, function(error,res){
-                if(error) throw console.error(error)   
+            connection.query(new_user, function(error,res){
+                if(error) throw console.error(error) 
+                var sql_resp;
                 for (let value of Object.values(res[0])) {
                     auth = value;
                 }
@@ -84,14 +74,16 @@ module.exports  = {
                         "Reason" : "Username or Password already exist"
                     }
                 }
-                console.log("Response: " + sql_resp);
-                callback(sql_resp);
+                console.log("Response: " + sql_resp)
+                connection.end()
+                callback(sql_resp)
             });
         }    
         else {
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
             }
+            connection.end()
             callback(error_resp)
         }
     },
@@ -99,7 +91,7 @@ module.exports  = {
         var all_surveys = 'CALL get_surveys();'
         var jsonresponse
         var array = []
-        query(all_surveys, connection, function(error, res){
+       connection.query(all_surveys, function(error, res){
             if (error) throw console.error(error);
             for (let value of Object.values(res[0])) {
                 array.push(value.survey_name)
@@ -107,16 +99,17 @@ module.exports  = {
             jsonresponse = {
                 "survey" : array
             }
+            connection.end()
             callback(jsonresponse)
         });
 
     },
     get_survey_questions: function(survey, connection, callback){
         if(survey != undefined){
-            var questions = 'CALL get_questions_by_survey("' + survey + '");';
+            var questions = 'CALL get_questions_by_survey("' + survey + '");'
             var suveyQuestions = {"Questions": []}
             jsonResponse="";
-            query(questions, connection, function(error,res){
+            connection.query(questions, function(error,res){
                 jsonResponse = ""
                 if(error) throw console.error(error);
                 for (let value of Object.values(res[0])) {
@@ -134,37 +127,51 @@ module.exports  = {
                     }
                     suveyQuestions['Questions'].push(temp)
                 }
-                callback(suveyQuestions);  
+                connection.end()
+                callback(suveyQuestions)
             });
         }
         else {
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
             }
+            connection.end()
             callback(error_resp)
         }       
     },
     send_answers: function(user, pass, connection, answers, callback){
         if(user != undefined && pass != undefined && answers != undefined ){  
             for(i = 0; i < answers.Questions.length; i++){
-                var answer = 'CALL insert_answer("' + user + '","' + pass + '",' +  answers.Questions[i].ID + ',"' + answers.Questions[i].UserAnswer + '");';
-                insert_query(answer, connection, function(res){
-                });
+                var answer = 'CALL insert_answer("' + user + '","' + pass + '",' +  answers.Questions[i].ID + ',"' + answers.Questions[i].UserAnswer + '");'
+                if(i == answers.Questions.length-1){
+                    connection.query(answer, function(error)
+                    {
+                        if(error) {
+                            console.error(error)
+                        }
+                        var insert_status = {
+                            "Process" : "Done",
+                        }
+                        connection.end()
+                        callback(insert_status)
+                    })
+                }
+                else{
+                    connection.query(answer, function(error)
+                    {
+                        if(error) {
+                            console.error(error)
+                        }
+                    })
+                }
             }
-            var done_rep = {
-                "Process" : "Done",
-            };
-            callback(done_rep);
         }
         else {
             var error_resp = {
                 "Invalid Response" : "Undefined Attributes"
             }
+            connection.end()
             callback(error_resp)
         }       
     },
-
-    send_profile_picture: function(user, connection, callback){
-        
-    }
 }
